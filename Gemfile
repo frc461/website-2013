39ea938240af66951d3c7b0990536937f865ebf6
Kristofer Rye <kristofer.rye@gmail.com>
2013-01-03 14:10:07 -0500

    changed the page render algorithms

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.9'
 gem 'sqlite3'
 gem 'twitter-bootstrap-rails'
 gem 'activeadmin'
+gem 'bluecloth'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+require 'bluecloth'
 module ApplicationHelper
+  def super_format(text)
+    
+  end
+
+  def format(text)
+    sanitize(markdown(text))
+  end
+
+  def markdown(text)
+    BlueCloth::new(text).to_html
+  end
 end
