be1ac14fcbe410b7640b2f05bc9cc1afbb22eddf
Kristofer Rye <kristofer.rye@gmail.com>
2013-01-03 16:42:49 -0500

    added group permissions now

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,10 @@
 class GroupsController < InheritedResources::Base
+  def add_permission
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @permission = Permission.new
+    @permissions = @group.permissions
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,14 @@
 class Group < ActiveRecord::Base
   attr_accessible :name
 
+  validates_presence_of :name
+  after_create :create_principal
   has_many :memberships
   has_many :users, :through => :memberships
   has_one :principal, :as => :authenticatable
+  has_many :permissions, :through => :principal
+
+  def create_principal
+    p = Principal.create :authenticatable_type => "Group", :authenticatable_id => self.id
+  end
 end
