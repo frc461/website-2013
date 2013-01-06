4eafdeddec57b39912e0cf54dba279c4ddcd6352
Kristofer Rye <kristofer.rye@gmail.com>
2013-01-05 22:57:54 -0500

    revert schema

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,8 @@ ActiveRecord::Schema.define(:version => 20130102200326) do
     t.boolean  "write"
     t.boolean  "remove"
     t.boolean  "execute"
-    t.integer  "securable_id", :limit => 255
+    t.integer  "securable_id"
+    t.string   "securable_type"
     t.integer  "priority"
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
