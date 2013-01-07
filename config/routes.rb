7e37fbef16536fca93a99f9a2ef0d519f9653c2b
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-01-06 22:01:02 -0500

    various other fixes
    User name instead of email
    fix /groups.1 instead of /groups/1 problem
    Probably other stuff too

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Website::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
-  get "groups" => "groups#new", :as => "group"
+  get "groups" => "groups#new", :as => "groups"
   get "users" => "users#index", :as => "users"
 
   # The priority is based upon order of creation:
