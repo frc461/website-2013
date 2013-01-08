c63a8bfa5472479542cc5dc9274b37c19a4c54e8
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-01-08 13:06:15 -0500

    add debug code

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -7,17 +7,25 @@ class Permission < ActiveRecord::Base
   belongs_to :principal
 
   def securify
-    if securable_string.include? "/" 
+    puts "about to start if including /"
+    if securable_string.include? "/"
+      puts "includes, splitting on /"
       type = securable_string.split("/")[0]
       id = securable_string.split("/")[1]
-    else 
+    else
+      puts "doesn't include"
       type = securable_string
     end
+    puts "next part"
 
     if ["Page", "Post", "Forum", "Photo", "Album"].include? type
+      puts "thing includes other things"
       securable_type = type
+      puts securable_type
       if securable_string.include?("/") && securable_type.constantize.where(:id => id)
+        puts "includes /, setting id"
         securable_id = id.to_i
+        puts securable_id
       else
         self.errors.add(:base, "You fail")
       end
