ce4694a805ca18eced082b2e302f0928a117c043
Kristofer Rye <kristofer.rye@gmail.com>
2013-01-09 20:18:13 -0500

    working on photos...
    you need to migrate your database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130102200326) do
+ActiveRecord::Schema.define(:version => 20130110002823) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -75,8 +75,12 @@ ActiveRecord::Schema.define(:version => 20130102200326) do
 
   create_table "photos", :force => true do |t|
     t.integer  "album_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "posts", :force => true do |t|
