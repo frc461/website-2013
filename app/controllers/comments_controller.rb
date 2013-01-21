ed1809a643cd0ebdfd5608b815ba9df1c1f48d32
Kristofer Rye <kristofer.rye@gmail.com>
2013-01-20 23:28:57 -0500

    changed a lot of things [expand]
    > code is prettier
    > rendering is less bloated, and I'm working on making things more compact overall
    (I'm trusting that somebody will fix areas where my judgement was wrong

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,21 @@
 class CommentsController < InheritedResources::Base
   load_and_authorize_resource
+
   def create
     @comment = Comment.new(params[:comment])
-    if @comment.save
+    if @comment.save && @comment.errors.count == 0
+      flash[:notice] = ["Created comment", @comment.id, "successfully"].join(" ")
       if @comment.parent_id
         redirect_to @comment.parent
       else
         redirect_to @comment
       end
     else
+      flash[:error] = @comment.errors.to_a.join(", ")
       if @comment.parent_id
-        render @comment.parent, :notice => "You screwed something up there. Check again."
+        redirect_to @comment.parent
       else
-        render :new, :notice => "You screwed something up there. Check again."
+        redirect_to :new
       end
     end
   end
