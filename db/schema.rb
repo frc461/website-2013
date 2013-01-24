e8f29784e4ce404bd8b788a0d127c036211b0ef4
Kristofer Rye <kristofer.rye@gmail.com>
2013-01-23 19:16:39 -0500

    made it so that users can't just join whatever group they want to when they create themselves

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130119191906) do
+ActiveRecord::Schema.define(:version => 20130123235646) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20130119191906) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "joinable"
   end
 
   create_table "memberships", :force => true do |t|
