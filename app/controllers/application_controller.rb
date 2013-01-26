f342c47755396fa4946c3ea95a532ae3d62f1f3b
samercier <merciers.merciers@gmail.com>
2013-01-26 16:59:50 -0500

    things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
 
+  def handle_error
+    redirect_to error_path
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+rescue_from Exception, :with => :handle_error
+  rescue_from ActionController::RoutingError, :with => :handle_error 
+
+#, ActionController::UnknownController, AbstractController::ActionNotFound, ActiveRecord::RecordNotFound  def handle_error
 
   private
   def current_user
