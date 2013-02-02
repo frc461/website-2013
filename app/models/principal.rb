8642a5c3ac16aa91e9824c569a3fe22a8e0be3dc
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-02-02 16:42:02 -0500

    add todo stuff

--- app/models/principal.rb
+++
@@ -1,6 +0,0 @@
-class Principal < ActiveRecord::Base
-  attr_accessible :authenticatable_id, :authenticatable_type
-
-  has_many :permissions
-  belongs_to :authenticatable, :polymorphic => true
-end
