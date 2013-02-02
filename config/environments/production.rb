a8d3b9b94399876183ca52a07394dc8618964e15
Brad Thompson <btgeek22@gmail.com>
2013-02-01 22:49:28 -0500

    Update config/environments/production.rb
    More deploy fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Website::Application.configure do
   config.action_controller.asset_host = "http://assets.boilerinvasion.org"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( search.js jquery.js jquery_ujs.js )
+  config.assets.precompile += %w( search.js jquery.js jquery_ujs.js fullcalendar.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
