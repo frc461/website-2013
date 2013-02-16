68703f2997f19baea3e8dc79bddc3de91b073df9
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-02-16 14:24:54 -0500

    page name validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,6 @@
 class Page < ActiveRecord::Base
   attr_accessible :content, :navigable, :parent_id, :title, :tag_list
+  validates_format_of :title, :with => /^[a-zA-Z0-9\- ]+$/
 
   has_many :pages, :foreign_key => :parent_id
   belongs_to :parent, :foreign_key => :parent_id, :class_name => :Page
