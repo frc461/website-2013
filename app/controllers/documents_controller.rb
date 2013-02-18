ff52c1cf977f86df051194cd2ede56c67d40c3be
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-02-18 16:05:17 -0500

    fix for external POST to /documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,15 @@
 class DocumentsController < InheritedResources::Base
+
+  def create 
+    @document = Document.new()
+    @document.name = params[:document][:name]
+    @document.filename = params[:document][:filename]
+    @document.secret_code = params[:document][:secret_code]
+    if @document.save
+      redirect_to documents_path
+    else
+      render :new
+    end
+  end
+  
 end
