db5204b3472422d9985439be25f41719e6ad5a08
samercier <merciers.merciers@gmail.com>
2013-03-21 15:06:40 -0400

    reimpliminted parallax scrolling w/o duplicating code

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -7,28 +7,28 @@
  *   @mohiuddinparekh
  */
 
-
 // PARALLAX SCROLLING
-/*$(document).ready(function(){
-  // Cache the Window object
-  $window = $(window);
-  $('section[data-type="background"]').each(function(){
-    var $bgobj = $(this); // assigning the object
-                    
-    $(window).scroll(function() {
-      // Scroll the background at var speed
-      // the yPos is a negative value because we're scrolling it UP!
-      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-    
-      // Put together our final background position
-      var coords = '50% '+ yPos + 'px';
+$(document).ready(function(){
+  if(document.documentElement.clientWidth > 979) {
+    // Cache the Window object
+    $window = $(window);
+    $('section[data-type="background"]').each(function(){
+      var $bgobj = $(this); // assigning the object
+                      
+      $(window).scroll(function() {
+        // Scroll the background at var speed
+        // the yPos is a negative value because we're scrolling it UP!
+        var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+      
+        // Put together our final background position
+        var coords = '50% '+ yPos + 'px';
 
-      // Move the background
-      $bgobj.css({ backgroundPosition: coords });
-    }); // window scroll Ends
- });
+        // Move the background
+        $bgobj.css({ backgroundPosition: coords });
+      }); // window scroll Ends
+    });
+  }
 });
-*/
 
 /* 
  * Create HTML5 elements for IE's sake
