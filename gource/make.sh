eb96fbf191a34da79c5b5dcdafe5a3f6814bb2de
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-03-21 01:50:16 -0400

    change gource stuff up

--- gource/make.sh
+++ gource/make.sh
@@ -1,6 +1,6 @@
 git log --format=format:"%at|%an: %s" --reverse > commit_log
 gource -s 1 \
-       --hide filenames,dirnames \
+       --hide filenames,dirnames,mouse,progress \
        --file-idle-time 0 \
        --max-file-lag 1 \
        -e .1 \
@@ -8,15 +8,26 @@ gource -s 1 \
        --caption-size 10 \
        --caption-duration 2 \
        -1920x1080 \
-       -r 30 \
+       -r 60 \
        -o - \
        .. \
 | \
 ffmpeg -y \
-       -r 40 \
+       -r 60 \
        -f image2pipe \
        -vcodec ppm \
        -i - \
-       -vcodec libvpx \
-       -b 20000K \
-       gource.webm
+       -vcodec libx264 \
+       -preset ultrafast \
+       -crf 1 \
+       -threads 0 \
+       -bf 0 \
+       gource.mp4
+# ffmpeg -y \
+#        -r 30 \
+#        -f image2pipe \
+#        -vcodec ppm \
+#        -i - \
+#        -vcodec libvpx \
+#        -b 1G \
+#        gource.webm
