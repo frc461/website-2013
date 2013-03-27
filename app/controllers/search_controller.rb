60839bfb0e5b90a7514e83dea8564be6f73ddbbd
computerguy505 <christopher.c.cooper@gmail.com>
2013-03-27 13:31:11 -0300

    fix up search a little

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,10 +8,10 @@ class SearchController < ApplicationController
     queries.each do |query|
       @pagethings += Page.tagged_with(query)
       @pagethings += Page.where("lower(title) LIKE lower(?)", "%" + query + "%")
-      @pagethings += Page.where("lower(content) LIKE lower(?)", "%" + query + "%")
+      #@pagethings += Page.where("lower(content) LIKE lower(?)", "%" + query + "%") Don't think this and below content checker really work unless we also filter out keywords like "in" or "and"
       @postthings += Post.tagged_with(query)
       @postthings += Post.where("lower(title) LIKE lower(?)", "%" + query + "%")
-      @postthings += Post.where("lower(content) LIKE lower(?)", "%" + query + "%")
+      #@postthings += Post.where("lower(content) LIKE lower(?)", "%" + query + "%") Or, we could search for these without seperated queries. I'll add that just in case.
       if current_user
         @eventhings += Event.where("lower(title) LIKE lower(?)", "%" + query + "%")
       else
@@ -20,6 +20,8 @@ class SearchController < ApplicationController
       @albuthings += Album.tagged_with(query)
       @albuthings += Album.where("lower(name) LIKE lower(?)", "%" + query + "%")
     end
+    @pagethings += Page.where("lower(content) LIKE lower(?)", "%" + params[:search] + "%")
+    @postthings += Post.where("lower(content) LIKE lower(?)", "%" + params[:search] + "%")
     @pagethings.uniq!
     @postthings.uniq!
     @eventhings.uniq!
