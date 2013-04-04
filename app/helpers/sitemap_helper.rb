6b8368b396721f8bc45e0ac0710bc549499aecc2
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-04-04 19:49:18 -0400

    sitemap heirarchies
    yay recursion!

--- app/helpers/sitemap_helper.rb
+++ app/helpers/sitemap_helper.rb
@@ -1,2 +1,14 @@
 module SitemapHelper
+
+  def page_ul page
+    thing = "<ul>"
+    page.pages.each do |child|
+      thing += "<li>"
+      thing += link_to_page child
+      thing += "</li>"
+      thing += page_ul(child) if child.pages.count > 0
+    end
+    thing += "</ul>"
+    return thing.html_safe
+  end
 end
