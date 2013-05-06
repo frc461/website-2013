e5de4111e79de47d73bbea56ddaf9fe04b97ab8c
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-05-05 20:44:38 -0400

    restrict assigning special things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,34 @@ class UsersController < InheritedResources::Base
   end
 
   def create
+    params[:user].delete(:admin) unless session[:user_id] && User.find(session[:user_id]).admin
+    if !(session[:user_id] && User.find(session[:user_id]).admin) && params[:user][:group_ids]
+      params[:user][:group_ids].delete_if do |gid|
+        !Group.find(gid).joinable
+      end
+    end
     @user = User.new(params[:user])
-    @user.group_ids = params[:group_ids]
+    # @user.group_ids = params[:group_ids]
     if @user.save
       redirect_to root_url, :notice => "Signed up!"
     else
       render "new"
+    end
+  end
+
+  def update
+    params[:user].delete(:admin) unless session[:user_id] && User.find(session[:user_id]).admin
+    if !(session[:user_id] && User.find(session[:user_id]).admin) && params[:user][:group_ids]
+      params[:user][:group_ids].delete_if do |gid|
+        !Group.find(gid).joinable
+      end
+    end
+    @user = User.find(params[:id])
+    # @user.group_ids = params[:group_ids]
+    if @user.update_attributes(params[:user])
+      redirect_to @user
+    else
+      render "edit"
     end
   end
 end
