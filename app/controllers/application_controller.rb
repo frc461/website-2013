3e54c1c76599ad01748b0af2e87ee96175ccbd77
Kristofer Rye <kristofer.rye@gmail.com>
2013-09-12 18:36:06 -0400

    Getting rid of complicated MD5-y stuff with Debug String
    We'll add just the commit string to the debug string because that's all I foresee us caring about.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,7 @@ class ApplicationController < ActionController::Base
 	
 	def debuggy
 		@print_debug = true
-		@debug_string = Digest::MD5.hexdigest(
-		                                      "{ \"" +
-		                                      (current_user ? current_user.name : "NOT LOGGED IN" ) +
-		                                      "\", \"" +
-		                                      (@print_debug ? "true" : "false") +
-		                                      "\" };")
+		@debug_string = Digest::MD5.hexdigest("{empty string};")
 	end
 
 	#rescue_from Exception, :with => :handle_error
