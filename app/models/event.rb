98e66bf62bfcf5d5568d32625ce39d05a41d4812
Sammidysam <sammidysam@gmail.com>
2014-01-05 17:45:34 -0500

    Validations for events!
    Now an event must always have a title, start_date, and end_date.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,21 +1,11 @@
 class Event < ActiveRecord::Base
 	attr_accessible :content, :end_date, :location, :public, :start_date, :title, :weeks_repeat, :end_repeat
 
+	validates :title, :start_date, :end_date, presence: true
+
 	before_save :check_for_errors
 
 	def check_for_errors
-		if self.title.length == 0
-			self.errors.add(:base, "Events must have titles!")
-		end
-
-		if !self.start_date
-			self.errors.add(:base, "Events must have start dates!")
-		end
-
-		if !self.end_date
-			self.errors.add(:base, "Events must have end dates!")
-		end
-
 		if self.start_date && self.end_date
 			# check for start_date before end_date
 		end
