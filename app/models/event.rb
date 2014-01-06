965ff3435895b954329b1e16ecffdcf330023496
Sammidysam <sammidysam@gmail.com>
2014-01-05 20:41:36 -0500

    Add comment, new lines

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ActiveRecord::Base
 	validates :weeks_repeat, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 	validate :end_date_cannot_be_before_start_date
 
+	# a < b where a and b are dates means that a is before b
 	def end_date_cannot_be_before_start_date
 		if end_date.present? && start_date.present? && end_date < start_date
 			errors.add(:end_date, "cannot be before start date")
@@ -13,11 +14,13 @@ class Event < ActiveRecord::Base
 	
 	def calendarify
 		r = {}
+		
 		r[:id] = self.id
 		r[:start] = self.start_date.iso8601
 		r[:end] = self.end_date.iso8601
 		r[:title] = self.title
 		r[:url] = "/events/#{id}"
+		
 		r
 	end
 end
