b9c44922c4fd5cfe7dfced6c43c1d5565f3c2f3d
Sammidysam <sammidysam@gmail.com>
2014-01-08 14:08:37 -0500

    Fix up ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,27 +2,33 @@ class Ability
 	include CanCan::Ability
 
 	def initialize(user)
-		if user
-			logged_in = true
-		end
-		user ||= User.new # guest user (not logged in)
+		logged_in = true if current_user
+
+		# Guest user.
+		user ||= User.new
+
+		# Universal permissions.
 		can :read, Page
 		can :read, Post
 		can :read, Album
 		can :read, Photo
 		can :create, User
 		can :read, Event
+		
 		if logged_in
-			can :update, user # lowercase is the user initalize is called on, not User (the class)
+			# Lowercase is the user initalize is called on, not the class User.
+			can :update, user
 			can :read, user
 			can :read, Forum
 			can :read, Comment
 			can :create, Comment
 			can :manage, Comment, :user_id => user.id
-			cannot :destroy, Comment
 			can :read, Event
 			can :read, Todo
 			can :write, Todo
+			
+			cannot :destroy, Comment
+			
 			page_access = false
 			post_access = false
 			photo_access = false
@@ -30,6 +36,7 @@ class Ability
 			user_access = false
 			forum_access = false
 			group_access = false
+			
 			user.groups.each do |g|
 				page_access  ||= g.page_access
 				post_access  ||= g.post_access
@@ -39,43 +46,49 @@ class Ability
 				forum_access ||= g.forum_access
 				group_access ||= g.group_access
 			end
+			
 			if page_access
 				can :manage, Page
+				
 				cannot :destroy, Page
 			end
 			
 			if post_access
 				can :manage, Post
+				
 				cannot :destroy, Post
 			end
 			
 			if photo_access
 				can :manage, Photo
-				cannot :destroy, Photo
 				can :manage, Album
+				
+				cannot :destroy, Photo
 				cannot :destroy, Album
 			end
 			
 			if user_access
 				can :manage, User
+				
 				cannot :destroy, User
 			end
 			
 			if forum_access
 				can :manage, Forum
-				cannot :destroy, Forum
 				can :manage, Comment
+				
+				cannot :destroy, Forum
 				cannot :destroy, Comment
 			end
 			
 			if group_access
 				can :manage, Group
+				
 				cannot :destroy, Group
 			end
 		end
-		
-		if user.admin
-			can :manage, :all
-		end
+
+		# Admins can do everything.
+		can :manage, :all if user.admin
 	end
 end
