4226faa37f4512546bbc9fb88a99e68d08915de2
Christopher Cooper <christopher.c.cooper@gmail.com>
2014-03-15 13:35:56 -0400

    Probably should have tested

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -11,10 +11,10 @@ class Photo < ActiveRecord::Base
 
 	def to_jq_upload
 		{
-			"name" => read_attribute(image_file_name),
-			"size": read_attribute(image_file_size),
-			"url": image.url(original),
-			"delete_url": photo_path(self),
+			"name" => read_attribute(:image_file_name),
+			"size" => read_attribute(:image_file_size),
+			"url" => image.url(:original),
+			"delete_url" => photo_path(self),
 			"delete_type" => "DELETE"
 		}
 	end
