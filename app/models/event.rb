d498501683d3b5de957f503f549062760e98800d
Kristofer Rye <kristofer@zeta>
2014-03-18 01:04:50 -0400

    models/event: Add some comments.
    I hope to add a lot of comments to this.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,13 @@
 class Event < ActiveRecord::Base
 	attr_accessible :content, :end_date, :location, :public, :start_date, :title, :weeks_repeat, :end_repeat, :color, :showtime
 
+	# Require title, start_date, and :end_date to be things.
 	validates :title, :start_date, :end_date, presence: true
+
+	# Require weeks_repeat to be only an integer that is greater than or equal to 0 (or blank)
 	validates :weeks_repeat, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
+
+	# Require end date to come after start date
 	validate :end_date_cannot_be_before_start_date
 
 	# a < b where a and b are dates means that a is before b
@@ -12,6 +17,7 @@ class Event < ActiveRecord::Base
 		end
 	end
 
+	# Convert this into a cute hash thing for fullcalendar.
 	def calendarify
 		r = {}
 
