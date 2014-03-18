411a8665227f0d0ab700d5abb48424c735f0a714
Kristofer Rye <kristofer@zeta>
2014-03-18 01:08:34 -0400

    models/group: Add a newline

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ActiveRecord::Base
 	attr_accessible :name, :joinable, :page_access, :post_access, :photo_access, :event_access, :user_access, :forum_access, :group_access
 
 	validates_presence_of :name
+
 	has_many :memberships
 	has_many :users, through: :memberships
 	has_many :todos
