312dfa078e78a684e438911056974c99bc3179e9
Kristofer Rye <kristofer@zeta>
2014-03-18 01:22:21 -0400

    models/user: Add a ton of comments
    Comments are good for the soul.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,46 +2,77 @@ require "bcrypt"
 class User < ActiveRecord::Base
 	attr_accessible :admin, :email, :name, :group_ids, :password, :password_confirmation, :password_hash, :password_salt, :secret_code
 	attr_accessor :password, :secret_code
+
+	# Run the encrypt_password and set_admin functions before saving
 	before_save :encrypt_password, :set_admin
-	
+
+	# The secret code should be given upon user creation and should match SECRET_CODE
 	validates_each :secret_code do |record, attr, value|
-		if record.secret_code != SECRET_CODE #defined in secret.rb
+		# SECRET_CODE should be defined in an initializer
+		if record.secret_code != SECRET_CODE
 			record.errors.add :secret_code, "is not correct"
 		end
 	end
-	
+
+	# TODO: Redo the validations to be more "modern"
+
+	# If we do any changing to password, we need to have a confirmation.
 	validates_confirmation_of :password
+
+	# We need to have a password (at least for creation)
 	validates_presence_of :password, on: :create
+
+	# We need to have an email given
 	validates_presence_of :email
-	validates_presence_of :name
+
+	# And the email needs to be unique
 	validates_uniqueness_of :email
+
+	# We need to have a name given
+	validates_presence_of :name
 
 	def set_admin
 		if self.admin == nil
 			self.admin = false
 		end
-		true
+
+		return true
 	end
 
+	# Gets all of the permissions for the user (inheritance from groups is accounted for)
 	def all_permissions
+		# Start with an empty array
 		shopping_cart = []
+
+		# Then add our user's specific permissions
 		shopping_cart.insert(self.permissions).flatten
+
+		# And then add all of the permissions for their groups
 		self.groups.each do |grp|
 			shopping_cart.insert(grp.permissions).flatten
 		end
-		shopping_cart
+
+		# :shipit:
+		return shopping_cart
 	end
 
+	# The authentification function
 	def self.authenticate(email, password)
+		# We find by email (hence the need for unique emails)
 		user = User.where(email: email).first
+
+		# If the user exists, check that their password_hash matches the hash created by hashing
+		# the given password with the password_salt, and return the user if they authenticate
+		# successfully (nil otherwise)
 		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-			user
+			return user
 		else
-			nil
+			return nil
 		end
 	end
 
 	def encrypt_password
+		# If we have a password, then generate a salt and hash the password with the salt.
 		if password.present?
 			self.password_salt = BCrypt::Engine.generate_salt
 			self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
