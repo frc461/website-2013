338cf05b7c5ba1bd0c7e780e4208714a83982463
Kristofer Rye <kristofer@zeta>
2014-03-18 01:04:17 -0400

    models/document: Add a newline
    This makes it less ugly.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,7 @@
 class Document < ActiveRecord::Base
 	attr_accessible :filename, :name, :secret_code
 	attr_accessor :secret_code
+
 	validates_each :secret_code do |record, attr, value|
 		if record.secret_code != DOCUMENT_SECRET_CODE #defined in secret.rb
 			record.errors.add :secret_code, "is not correct"
