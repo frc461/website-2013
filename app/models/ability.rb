a6e1d94bea9b83b8d7b92700db07a30d917bb3db
Kristofer Rye <kristofer@zeta>
2014-03-18 01:00:12 -0400

    Clean up abilities a bit
    Most of this was actually me alphabetizing these things. Nothing
    special, and I may change this later depending on preference. (I may
    group things differently)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,76 +5,89 @@ class Ability
 		if user
 			logged_in = true
 		end
-		user ||= User.new # guest user (not logged in)
-		can :read, Page
-		can :read, Post
-		can :read, Album
-		can :read, Photo
+
+		# Guest user for if the current visitor is not logged in.
+		user ||= User.new
+
 		can :create, User
+
+		can :read, Album
 		can :read, Event
+		can :read, Page
+		can :read, Photo
+		can :read, Post
+
 		if logged_in
-			can :update, user # lowercase is the user initalize is called on, not User (the class)
-			can :read, user
-			can :read, Forum
-			can :read, Comment
 			can :create, Comment
-			can :manage, Comment, user_id: user.id
+
 			can :destroy, Comment
 			cannot :destroy, Comment, parent_id: nil
+
+			can :manage, Comment, user_id: user.id
+
+			can :read, Comment
 			can :read, Event
+			can :read, Forum
 			can :read, Todo
+			can :read, user
+
+			can :update, user
+
 			can :write, Todo
-			page_access = false
-			post_access = false
-			photo_access = false
+
 			event_access = false
-			user_access = false
 			forum_access = false
 			group_access = false
+			page_access = false
+			photo_access = false
+			post_access = false
+			user_access = false
+
 			user.groups.each do |g|
-				page_access  ||= g.page_access
-				post_access  ||= g.post_access
-				photo_access ||= g.photo_access
 				event_access ||= g.event_access
-				user_access  ||= g.user_access
 				forum_access ||= g.forum_access
 				group_access ||= g.group_access
+				page_access  ||= g.page_access
+				photo_access ||= g.photo_access
+				post_access  ||= g.post_access
+				user_access  ||= g.user_access
 			end
-			if page_access
-				can :manage, Page
-				cannot :destroy, Page
-			end
-			
-			if post_access
-				can :manage, Post
-				cannot :destroy, Post
-			end
-			
-			if photo_access
-				can :manage, Photo
-				cannot :destroy, Photo
-				can :manage, Album
-				cannot :destroy, Album
-			end
-			
-			if user_access
-				can :manage, User
-				cannot :destroy, User
-			end
-			
+
 			if forum_access
 				can :manage, Forum
 				cannot :destroy, Forum
 				can :manage, Comment
 				cannot :destroy, Comment
 			end
-			
+
 			if group_access
 				can :manage, Group
 				cannot :destroy, Group
 			end
+
+			if page_access
+				can :manage, Page
+				cannot :destroy, Page
+			end
+
+			if photo_access
+				can :manage, Photo
+				cannot :destroy, Photo
+				can :manage, Album
+				cannot :destroy, Album
+			end
+
+			if post_access
+				can :manage, Post
+				cannot :destroy, Post
+			end
+
+			if user_access
+				can :manage, User
+				cannot :destroy, User
+			end
 		end
-		
+
 		if user.admin
 			can :manage, :all
 		end
