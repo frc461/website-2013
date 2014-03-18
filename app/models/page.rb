7861fc390e0cf5f5bfcfb540c8ad76fb20daa3df
Kristofer Rye <kristofer@zeta>
2014-03-18 01:13:28 -0400

    models/page: Adding some newlines.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,10 @@
 class Page < ActiveRecord::Base
 	attr_accessible :content, :navigable, :parent_id, :title, :tag_list, :sort_index
+
 	validates_format_of :title, with: /^[a-zA-Z0-9\-: ]+$/
 	validates_presence_of :sort_index
 	validates_uniqueness_of :title
+
 	has_many :pages, foreign_key: :parent_id
 	belongs_to :parent, foreign_key: :parent_id, class_name: :Page
 
