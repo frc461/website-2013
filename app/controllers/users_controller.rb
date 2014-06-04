80f9b316473de0a0f1480c43016147a6e67c64a4
Kristofer Rye <kristofer.rye@gmail.com>
2014-06-03 22:14:13 -0400

    users/destroy: Check if logged in, log out if logged in as condemned.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < InheritedResources::Base
 
 		if current_user
 			@groups += current_user.groups
-			
+
 			@groups.uniq!
 		end
 	end
@@ -23,26 +23,26 @@ class UsersController < InheritedResources::Base
 
 	def create
 		params[:user].delete(:admin) unless can? :manage, User
-		
+
 		if !(current_user && current_user.admin) && params[:user][:group_ids]
 			params[:user][:group_ids].delete_if do |gid|
 				!Group.find(gid).joinable
 			end
 		end
-		
+
 		@user = User.new(params[:user])
 
 		if (group = Group.where(name: "Everybody")).count > 0
 			group.first.users << @user
 		end
-		
+
 		if @user.save
 			if !(current_user && current_user.admin)
 				redirect_to root_url, notice: "Signed up!"
 			else
 				redirect_to root_url, notice: "New user created!"
 			end
-			
+
 			session[:user_id] = @user.id if !(current_user && current_user.admin)
 		else
 			if current_user && current_user.admin
@@ -67,7 +67,7 @@ class UsersController < InheritedResources::Base
 		else
 			@groups = Group.where(joinable: true)
 		end
-		
+
 		if current_user
 			@groups += current_user.groups
 			@groups.uniq!
@@ -76,15 +76,15 @@ class UsersController < InheritedResources::Base
 
 	def update
 		params[:user].delete(:admin) unless can? :manage, User
-		
+
 		if !(current_user && current_user.admin) && params[:user][:group_ids]
 			params[:user][:group_ids].delete_if do |gid|
 				!(Group.find(gid).joinable || (current_user && Membership.where(user_id: current_user.id, group_id: gid).count > 0))
 			end
 		end
-		
+
 		@user = User.find(params[:id])
-		
+
 		if @user.update_attributes(params[:user])
 			redirect_to @user
 		else
@@ -103,8 +103,11 @@ class UsersController < InheritedResources::Base
 	end
 
 	def destroy
-		# Log out before deleting.
-		session[:user_id] = nil
+		# If logged in as the user being deleted.
+		if session[:user_id] == @user.id
+			# Log out before deleting.
+			session[:user_id] = nil
+		end
 
 		@user.destroy
 
